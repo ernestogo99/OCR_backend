60947fa95d0b04a17d518be33ab6f210c396a487
Ernesto Dalva de Medeiros <ernesto.medeiros@great.local.ufc.br>
2025-12-14 15:50:26 -0300

    auth finished

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
+
+import { ApiOperation } from '@nestjs/swagger';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @ApiOperation({ summary: 'Endpoint para fazer login' })
+  @Post()
+  signIn(@Body() createAuthDto: CreateAuthDto) {
+    return this.authService.signIn(createAuthDto);
+  }
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,47 @@
+import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { ApiBearerAuth, ApiBody, ApiOperation } from '@nestjs/swagger';
+import { CurrentUser } from 'src/auth/currentUser';
+import { JwtAuthGuard } from 'src/auth/jwtauthguard';
+
+import { UserResponseDTO } from './dto/user-response-dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @ApiOperation({ summary: 'Criar um novo usuário' })
+  @ApiBody({
+    type: CreateUserDto,
+    description: 'Objeto de criação para um novo usuário',
+  })
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  @ApiOperation({ summary: 'Obter o usuário logado' })
+  @Get('me')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
+  getProfile(@CurrentUser() user: UserResponseDTO) {
+    return user;
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @ApiOperation({ summary: 'Obter um usuário por email' })
+  @Get(':email')
+  findByEmail(@Param('email') email: string) {
+    return this.usersService.findByEmail(email);
+  }
+
+  @ApiOperation({ summary: 'Obter um usuário por id' })
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.usersService.findOne(id);
+  }
+}
