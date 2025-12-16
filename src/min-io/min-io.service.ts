7f254325c2a52c1c6b4c51b1e1cc02fff707acdf
Ernesto Dalva de Medeiros <ernesto.medeiros@great.local.ufc.br>
2025-12-16 20:48:22 -0300

    s3

--- src/min-io/min-io.service.ts
+++ src/min-io/min-io.service.ts
@@ -10,11 +10,11 @@ import { File } from 'multer';
 @Injectable()
 export class MinIoService {
   private s3 = new S3Client({
-    endpoint: `http://${process.env.MINIO_ENDPOINT}:${process.env.MINIO_PORT}`,
     region: 'us-east-1',
     credentials: {
-      accessKeyId: process.env.MINIO_ACCESS_KEY!,
-      secretAccessKey: process.env.MINIO_SECRET_KEY!,
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+      secretAccessKey: process.env.AWS_SECRET_ACESS_KEY!,
+      sessionToken: process.env.AWS_SESSION_TOKEN,
     },
     forcePathStyle: true,
   });
@@ -24,7 +24,7 @@ export class MinIoService {
 
     await this.s3.send(
       new PutObjectCommand({
-        Bucket: process.env.MINIO_BUCKET!,
+        Bucket: process.env.AWS_S3_BUCKET,
         Key: key,
         Body: file.buffer,
         ContentType: file.mimetype,
@@ -36,7 +36,7 @@ export class MinIoService {
 
   async getFileUrl(key: string): Promise<string> {
     const command = new GetObjectCommand({
-      Bucket: process.env.MINIO_BUCKET!,
+      Bucket: process.env.AWS_S3_BUCKET,
       Key: key,
     });
 
@@ -46,7 +46,7 @@ export class MinIoService {
   async downloadFile(key: string): Promise<StreamableFile> {
     const response = await this.s3.send(
       new GetObjectCommand({
-        Bucket: process.env.MINIO_BUCKET!,
+        Bucket: process.env.AWS_S3_BUCKET,
         Key: key,
       }),
     );
